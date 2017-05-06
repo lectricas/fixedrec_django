a5b2fb983315f424d94727dc617f0c026282f7b9
apoliusov <apoliusov@65apps.com>
2017-05-06 20:39:20 +0400

    adopted to higher django version

--- fixedrec/settings.py
+++ fixedrec/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '9_+6rxx_vx5kdi3)76=#us63af(=541=xdq2v&j#=yh(tu3r^q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fixedrec.pythonanywhere.com']
+ALLOWED_HOSTS = ['fixedrec.pythonanywhere.com', '127.0.0.1', '10.0.2.2']
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
